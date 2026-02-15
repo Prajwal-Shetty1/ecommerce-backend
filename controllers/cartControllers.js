94e06a04ed570d4536f1f292c34e02fe439fe919
Prajwal-Shetty1 <prajwalshetty7975@gmail.com>
2026-02-15 17:44:28 +0530

    Backend:Fourth Commit

---
+++ controllers/cartControllers.js
@@ -0,0 +1,65 @@
+import userModel from "../models/userModels.js";
+
+//Add products to UserCart
+const addToCart = async (req, res) => {
+    try {
+        const { itemId, size } = req.body;
+        const userId = req.userId;
+
+        const userData = await userModel.findById(userId);
+        let cartData = userData.cartData || {};
+
+        if (!cartData[itemId]) cartData[itemId] = {};
+        cartData[itemId][size] = (cartData[itemId][size] || 0) + 1;
+
+        await userModel.findByIdAndUpdate(userId, { cartData });
+
+        res.json({ success: true, message: "Added To Cart!" });
+    } catch (error) {
+        res.json({ success: false, message: error.message });
+    }
+};
+
+
+
+
+
+//Update User Cart
+const updateCart = async (req, res) => {
+  try {
+    const { itemId, size, quantity } = req.body;
+    const userId = req.userId;
+
+    const userData = await userModel.findById(userId);
+    let cartData = userData.cartData || {};
+
+    if (quantity === 0) {
+      delete cartData[itemId][size];
+      if (Object.keys(cartData[itemId]).length === 0) {
+        delete cartData[itemId];
+      }
+    } else {
+      cartData[itemId][size] = quantity;
+    }
+
+    await userModel.findByIdAndUpdate(userId, { cartData });
+    res.json({ success: true, message: "Cart Updated!" });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+
+//Get User Cart Data
+const getUserCart = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const userData = await userModel.findById(userId);
+    res.json({ success: true, cartData: userData.cartData || {} });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+
+export { addToCart, updateCart, getUserCart }
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
 import userRouter from './routes/userRoutes.js'
 import productRouter from './routes/productRoutes.js'
+import cartRouter from './routes/cartRoutes.js'
 
 const app = express()
 const port = process.env.PORT || 4000
@@ -19,7 +20,7 @@ app.use(cors())
 //API endpoints
 app.use('/api/user',userRouter);
 app.use('/api/product',productRouter);
-
+app.use('/api/cart',cartRouter);
 
 app.get('/', (req, res) => {
   res.send("API working!...")
